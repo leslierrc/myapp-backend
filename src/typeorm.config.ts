616d791aa60309f25faa837f2ed2eedaaad65b8f
leslierrc <leslierrodriguezcontrera25@gmail.com>
2025-08-20 16:11:33 -0400

    finallll

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -5,8 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  url: process.env.MYSQL_PUBLIC_URL || process.env.DATABASE_URL,
-  synchronize: true, // ⚠️ Solo en desarrollo
+  url: process.env.DATABASE_URL,
+  synchronize: false, // ⚠️ No en producción
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  logging: false,
 });
